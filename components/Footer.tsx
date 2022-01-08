ead9154a02e5eb6f4327adfe92a67a927764b3c0
David Ramirez <alejoramirezrios@gmail.com>
2022-01-08 17:13:55 -0500

    agregar tailwind, aspecto página NOSOTROS

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+   return (
+      <div className='w-full bottom-0 m-0 p-7 bg-[#BCE6E5] '>
+         Este es el Footer
+      </div>
+   )
+}
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+   return (
+      <div className='m-0 p-7 bg-[#43BEDD]' >
+         Este es el Navbar
+      </div>
+   )
+}
+
+export default Navbar;
