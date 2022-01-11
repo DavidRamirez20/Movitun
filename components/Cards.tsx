1d9e1572452d57e26f11a707e24d39d86456e600
David Ramirez <alejoramirezrios@gmail.com>
2022-01-10 23:41:56 -0500

    Arreglo errores de Script, Image

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 interface CardsType {
    name: string;
@@ -10,9 +11,9 @@ interface CardsType {
 const Cards = (cardType: CardsType) => {
    return (
       <li className='m-10 bg-cyan-200 shadow-md rounded-lg hover:cursor-pointer hover:scale-[1.02]'>
-         <Link href={cardType.link}>
+         <Link href={cardType.link} passHref>
             <div className='flex w-[240px] h-[350px]'>
-               <img src={cardType.image} alt={cardType.name} className='rounded-t-lg'/>
+               <Image src={cardType.image} alt={cardType.name} className='rounded-t-lg'/>
             </div>
          </Link>
          <span className="flex text-white m-5 font-bold text-xl justify-center">{cardType.name}</span>
